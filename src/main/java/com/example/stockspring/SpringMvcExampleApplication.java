7ee9cc64d12cc0af8e369ff95247b126ac6e60e1
murali <connectreactmurali@gmail.com>
2019-08-07 13:05:17 +0530

    adding company functionality

--- src/main/java/com/example/stockspring/SpringMvcExampleApplication.java
+++ src/main/java/com/example/stockspring/SpringMvcExampleApplication.java
@@ -1,4 +1,4 @@
-package com.example.springMvcExample;
+package com.example.stockspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
