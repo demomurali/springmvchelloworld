5407f65b7d1bd08738562837a6db4da21deeef5e
murali <connectreactmurali@gmail.com>
2019-08-07 10:25:47 +0530

    first commit

---
+++ src/main/java/com/example/springMvcExample/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.springMvcExample;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(path="/hello")
+	public String sayHello(){
+		System.out.println("hello");
+		return "helloWorld";
+	}
+	
+}
