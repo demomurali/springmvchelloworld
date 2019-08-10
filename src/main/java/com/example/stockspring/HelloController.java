6f8956cba559533d541bce68041af97925fd1c43
murali <connectreactmurali@gmail.com>
2019-08-10 12:57:08 +0530

    added spring rest

--- src/main/java/com/example/stockspring/HelloController.java
+++ src/main/java/com/example/stockspring/HelloController.java
@@ -4,17 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping(path="/hi")
 public class HelloController {
 
-	@RequestMapping(path="/hello")
-	public String sayHello(){
+	@RequestMapping(path="/employeeId/{id}")
+	@ResponseBody
+	public String sayHello(@PathVariable int id){
 		System.out.println("hello");
-		return "helloWorld";
+		return id+": I want to seee this content in the browser";
 	}
+	
+	
+	
 	
 	@RequestMapping(value="/values0")
 	public ModelAndView getEmployeeList0(){
